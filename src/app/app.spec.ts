b1f78207ea9a044c8149f9ad46e6d203a3fb2b78
Cosimo Corvino <68302018+Crow0197@users.noreply.github.com>
2026-09-19 02:00:02 +0200

    Update app.spec.ts

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,12 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { App } from './app';
+import { provideFirebase } from './core/firebase/firebase.providers';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideRouter([]), provideServiceWorker('ngsw-worker.js', { enabled: false })],
+      providers: [
+        provideRouter([]),
+        provideServiceWorker('ngsw-worker.js', { enabled: false }),
+        provideFirebase(),
+      ],
     }).compileComponents();
   });
 
